57741d72165e5197e56f32e9d3eb9065d69b0570
danielcselenyi <daniel.cselenyi@gmail.com>
2025-05-16 11:02:35 +0200

    set up build.gradle.kts and simplified main

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    application
 }
 
 group = "org.example"
@@ -11,6 +12,14 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // Selenium
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.20.0")
+
+    // WebDriver manager for auto-download of ChromeDriver, etc.
+    testImplementation("io.github.bonigarcia:webdrivermanager:5.8.0")
+
+    // JUnit 5
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 }
 
 tasks.test {
